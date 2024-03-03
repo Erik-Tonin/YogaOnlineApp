3814bc746982be0be95efc623b93080b0ba78344
tonin.dev <eriktonin.dev@gmail.com>
2024-03-03 19:24:05 -0300

    Arrumando a tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { UserModule } from './user/user.module';
 import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 import { LoginModule } from './login/login.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login.routing';
 import { UserService } from '../user/services/user.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
+    ReactiveFormsModule 
   ],
   providers: [UserService],
   exports: [
